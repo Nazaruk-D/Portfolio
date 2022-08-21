6a7565806012762a8cbe9d3dc898aa823904e9ad
Nazaruk-D <nazaruk-dima@mail.ru>
2022-08-21 17:29:14 +0300

    Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
 
 export default App;

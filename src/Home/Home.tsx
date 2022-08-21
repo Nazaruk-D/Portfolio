f7fc04078cf5ed966e67c586b6d50bc8a094c5b6
Nazaruk-D <nazaruk-dima@mail.ru>
2022-08-21 18:41:08 +0300

    Home page

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import s from "./Home.module.css"
+import sContainer from "./../common/styles/Container.module.css"
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className={s.HomeBlock}>
+            <div className={sContainer.container}>
+                <div className={s.text}>
+                    <span>Hi there</span>
+                    <h1>I am Dmitry Nazaruk</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}></div>
+            </div>
         </div>
     );
 };
